92655ee9b464a6ba93051e3ef19bc430256b1655
ponomarenko.v <valera14@mail.ru>
2019-08-25 15:13:12 +0300

    Updates

---
+++ precache-manifest.d241e5c09a626574d5315daa8a53e805.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "839bef6653a50f998fea7c5383b5e822",
+    "url": "/reshalka/index.html"
+  },
+  {
+    "revision": "a64b67d7227697d6a3f8",
+    "url": "/reshalka/static/css/main.ec469560.chunk.css"
+  },
+  {
+    "revision": "b2b0136a41515b186bf2",
+    "url": "/reshalka/static/js/2.91e8b6a5.chunk.js"
+  },
+  {
+    "revision": "a64b67d7227697d6a3f8",
+    "url": "/reshalka/static/js/main.9f08f751.chunk.js"
+  },
+  {
+    "revision": "a79fff63f2707394a8f0",
+    "url": "/reshalka/static/js/runtime~main.4fe2946b.js"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/reshalka/static/media/Montserrat-Medium.c8b6e083.ttf"
+  }
+]);
